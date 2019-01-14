0586303786b0edd0e5a8f99258abf4088f23eea8
ryan <hy10mhs@gmail.com>
2019-01-14 11:22:44 +0900

    countingSort stable

--- Sorting/countingSort.js
+++ Sorting/countingSort.js
@@ -12,7 +12,7 @@ const countingSort = function (arr){
     count[key] = acc;
   }
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     _arr[--count[arr[i]]] = arr[i];
   }
   
