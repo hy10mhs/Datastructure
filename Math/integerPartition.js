8f080f627960d50f7487ea4c7957e9997b6a0fd8
ryan <hy10mhs@gmail.com>
2019-01-25 14:18:56 +0900

    integer Partition

--- Math/integerPartition.js
+++ Math/integerPartition.js
@@ -1,15 +1,30 @@
-// 다시 해보자 :)
-function integerPartition(number, coins) {
-  const matrix = Array(coins.length).fill(null).map(() => Array(number + 1).fill(null));
+function integerPartition(number) {
+  const matrix = Array(number + 1).fill(null).map(() => Array(number+1).fill(null));
 
-  for (let i = 0; i < coins.length; i++) {
-    for (let j = coins[i]; j <= number; j++) {
-      if (i === 0 && number % coins[0] === 0) {
-        matrix[i][j] = 1;
+  // row: sum, column: summand, element: number of cases
+  //   X 
+  // Y     0  1  2
+  //   0 [ 1, 0, 0 ]
+  //   1 [ 1, 1, 1 ]
+  //   2 [ 1, 1, 2 ]
+
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i][0] = 1;
+  }
+
+  for (let i = 1; i < number + 1; i++) {
+    matrix[0][i] = 0;
+  }
+
+  for (let index = 1; index < number + 1; index++) {
+    for (let summandIndex = 1; summandIndex < number + 1; summandIndex++) {
+      if (index < summandIndex) {
+       matrix[summandIndex][index] = matrix[summandIndex-1][index];
       } else {
-        matrix[i][j] = (matrix[i-1][j] || 0) + (matrix[i][j-coins[i]] || 1);
+       matrix[summandIndex][index] = matrix[summandIndex-1][index] + matrix[summandIndex][index - summandIndex];
       }
     }
   }
-  console.log(matrix);
+
+  return matrix[number][number];
 }
